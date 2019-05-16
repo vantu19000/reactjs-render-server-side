63cfb88e6375dcfc45e4e5cef57d9a16951459c0
vantu19000 <vantu19000@gmail.com>
2019-05-16 07:49:51 +0700

    update

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 var React = require('react');
 import { Provider } from 'react-redux'
 var routes = require('./src/routes/Routes.js');
-import store from 'src/store/index';
+import store from './src/store';
 
 
 ReactDOM.render(
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,13 @@ import { StaticRouter } from "react-router";
 var Redux = require('redux');
 import { Provider } from 'react-redux'
 var NotFound = require('../views/layout/404.js');
-const store = require('../store');
-
+import store from '../store';
 
 function reducer(state) { return state; }
 
 router.get('*', function(request, response) {
+
+    const context = {}
 
     const jsx =
         <Provider store={ store }>
@@ -19,7 +20,6 @@ router.get('*', function(request, response) {
             </StaticRouter>
         </Provider>;
 
-    const context = {}
     const content = ReactDOMServer.renderToString(jsx);
     const html = `<html>${content}</html>`;
 
