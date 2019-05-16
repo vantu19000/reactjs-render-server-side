91a49195b7bc3977d0980f476842d120fee3af69
tu.nguyen.van <tu.nguyen.van@socialfintech.net>
2019-05-16 15:24:20 +0700

    update package

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/ping', (req, res) => {
     return res.send('pong');
 });
-
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 app.listen(port);
